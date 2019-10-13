66cb807cc00c35c723a95eac2f38c8613efe6d54
Junwoo Cheong <jcheong199569@gmail.com>
2019-10-13 10:52:26 -0400

    lab exercise

--- week7_lab/script.js
+++ week7_lab/script.js
@@ -1,69 +1,65 @@
 function Homo_sapiens(name, age) {
   this.name = name;
   this.age = age;
-  this.type = “Homo_sapiens”;
-  this.image = “Homo_Sapiens.jpg”;
+  this.type = "Homo_sapiens";
+  this.image = "Homo_sapiens.jpg";
 }
 
 function Homo_erectus(name, age) {
   this.name = name;
   this.age = age;
-  this.type = “Homo_erectus”;
-  this.image = “Homo_erectus.jpg”;
+  this.type = "Homo_erectus";
+  this.image = "Homo_erectus.jpg";
 }
 
 function Homo_something(name, age) {
   this.name = name;
   this.age = age;
-  this.type = “Homo_something”;
-  this.image = “Homo_something.jpg”;
+  this.type = "Homo_something";
+  this.image = "Homo_something.jpg";
 }
 
 var animals=[new Homo_sapiens(), new Homo_erectus(), new Homo_something()];
 
-var names=[Griffin, Manjun, Junwoo, Sam, Smith, John, Kevin, Kyle, Bro]; 
+var names=["Griffin", "Manjun", "Junwoo", "Sam", "Smith", "John", "Kevin", "Kyle", "Bro"]; 
 
-function generateRandomIndex(3) {
-    return Math.floor(Math.random() * 3);
+function generateRandomIndex(maxIndex) {
+    return Math.floor(Math.random() * maxIndex);
 }
 
-
-function getRandomIndex(maxIndex) {
-  return Math.floor(Math.random() * maxIndex);
-}
 
 // generates either a Cat, Dog, or Bird with a random name and random age
 function generateRandomAnimal() {
-  var randomIdx = getRandomIndex(animals.length);
+  var randomIdx = generateRandomIndex(animals.length);
   var randomAnimal = animals[randomIdx];
 
   if (randomAnimal instanceof Homo_sapiens) 
   {
-    return new Cat(generateRandomName(), generateRandomAge());
+    return new Homo_sapiens(generateRandomName(), generateRandomAge());
   } 
-  else if (randomAnimal instanceof Dog) 
+  else if (randomAnimal instanceof Homo_erectus) 
   {
-    return new Dog(generateRandomName(), generateRandomAge());
+    return new Homo_erectus(generateRandomName(), generateRandomAge());
   } 
-  else if (randomAnimal instanceof Bird) 
+  else if (randomAnimal instanceof Homo_something) 
   {
-    return new Bird(generateRandomName(), generateRandomAge());
+    return new Homo_something(generateRandomName(), generateRandomAge());
   }
 }
 
 // generates a random name from list of names
 function generateRandomName() {
-  var randomIdx = getRandomIndex(names.length);
+  var randomIdx = generateRandomIndex(names.length);
   return names[randomIdx];
 }
 
 // generates a random age from 0 to 5
 function generateRandomAge() {
-  var randomIdx = getRandomIndex(5);
+  var randomIdx = generateRandomIndex(5);
   return randomIdx;
 }
 
-/*** Document Load ****/
+
 function onLoad() {
 
   // generate a random animal when the document opens
