108ef7305fec903b2a321371daa53e09f9e385a7
Junwoo Cheong <jcheong199569@gmail.com>
2019-10-23 23:47:30 -0400

    update

--- assgn6/script.js
+++ assgn6/script.js
@@ -1,20 +1,42 @@
+var numItems=0
+var shoppingCart=new Object
+
+
+
+function addToCart(){
+    alert("Your tasty item has been added to your shopping cart!");
+    
+
+}
+
+
+
 
 function removeCartItem(){
     var x = document.getElementsByClassName("delete-btn");
     remove.x;
 }
 
+
 function priceFunction(){
     var x = document.getElementById("qt").value;
-    var price = document.getElementById("number").innerHTML;
+    // var price = document.getElementById("number").innerHTML;
     document.getElementById("number").innerHTML = `${parseInt(x)*2.5}$`;
 
+}
+
+function priceFunction2(){
+    var x = document.getElementById("glazing").value;
+    var y = document.getElementById("qt").value;
+    var z = document.getElementById("number").value;
+    var price = document.getElementById("number").innerHTML;
+    document.getElementById("number").innerHTML = `${parseInt(x)*parseInt(y)+parseInt(z)}$`;
 }
 
 function shoppingcartItemPrice(){
     var x = document.getElementById("cart-item-quantity").value;
     var itemprice = document.getElementById("total-price").innerHTML;
-    document.getElementById("total-price").innerHTML = `${parseInt(x)*2.5}$`;
+    document.getElementById("total-price").innerHTML = `${parseInt(x)*7}$`;
 }
 
 function glazingPrice(){
