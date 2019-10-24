d7792b4997df463d5076df03bbedc86fdd8fdbc3
Junwoo Cheong <jcheong199569@gmail.com>
2019-10-23 21:51:32 -0400

    some change

--- assgn6/script.js
+++ assgn6/script.js
@@ -1,9 +1,25 @@
-console.log('javascript is so hard wtf');
 
-var removeItemButton = document.getElementsByClassName('remove-button')
+function removeCartItem(){
+    var x = document.getElementsByClassName("delete-btn");
+    remove.x;
+}
+
 function priceFunction(){
     var x = document.getElementById("qt").value;
     var price = document.getElementById("number").innerHTML;
-    document.getElementById("number").innerHTML = `${parseInt(x)*4.5}$`
+    document.getElementById("number").innerHTML = `${parseInt(x)*2.5}$`;
 
+}
+
+function shoppingcartItemPrice(){
+    var x = document.getElementById("cart-item-quantity").value;
+    var itemprice = document.getElementById("total-price").innerHTML;
+    document.getElementById("total-price").innerHTML = `${parseInt(x)*2.5}$`;
+}
+
+function glazingPrice(){
+    var x = document.getElementsByClassName("glazing-type").value;
+    var y = document.getElementById("cart-item-quantity").value;
+    var galzingprice = document.getElementById("total-price").innerHTML;
+    document.getElementById("total-item").innerHTML = `${parseInt(x)*parseInt(y)}$`
 }
