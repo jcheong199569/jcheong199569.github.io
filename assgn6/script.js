c7c431114c936c993224f555a93a40d1c2e5089b
Junwoo Cheong <jcheong199569@gmail.com>
2019-10-30 09:42:23 -0400

    -A kill me. change on glazing option from radio buttons to select for consistency to shopping cart

--- assgn6/script.js
+++ assgn6/script.js
@@ -30,18 +30,12 @@ function removeCartItem(){
 
 function priceFunction(){
     var x = document.getElementById("qt").value;
-    // var price = document.getElementById("number").innerHTML;
-    document.getElementById("number").innerHTML = `${parseInt(x)*2.5}$`;
+    var y = document.getElementById("glazing").value;
+    document.getElementById("number").innerHTML = `${(x*2.5) + (x*y)}$`;
 
 }
 
-function priceFunction2(){
-    var x = document.getElementById("glazing").value;
-    var y = document.getElementById("qt").value;
-    var z = document.getElementById("number").value;
-    // var price = document.getElementById("number").innerHTML;
-    document.getElementById("number").innerHTML = `${parseInt(x)*parseInt(y)+parseInt(z)}$`;
-}
+
 
 function shoppingcartItemPrice(){
     var x = document.getElementById("cart-item-quantity").value;
