6b718db715eb2d8b2236f56c60a52d94ef19bab6
Junwoo Cheong <jcheong199569@gmail.com>
2019-12-05 02:33:55 -0500

    update

--- final_project/js/script.js
+++ final_project/js/script.js
@@ -1,6 +1,10 @@
-function myFunction() {
+function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
+    var logo = document.getElementById("logo");
+    logo.src="asset/SVG/logo-yellow.SVG";
+    var navItem = document.getElementsByClassName("nav-link");
+    // navItem.
 }
 
 var app = document.getElementById('app');
@@ -9,14 +13,25 @@ var typewriter = new Typewriter(app, {
     loop: true
 });
 
-typewriter.typeString(JSON.stringify(app))
+
+typewriter.typeString("Hi, I am Junwoo.")
     .start()
-    .pauseFor(2500)
-    .deleteAll()
-    .start();
+    .pauseFor(10000);
 
 $(document).ready(function(){
     $("#flip").click(function(){
     $("#panel").slideToggle("slow");
 });
 });
+
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter2() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
